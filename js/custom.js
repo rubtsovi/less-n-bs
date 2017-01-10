22ca37826d846fdb8d0d697a99c034d162e9f21f
rubtsovi <rubtsovi.90@gmail.com>
2017-01-10 22:48:08 +0100

    BS & LESS strefa kursow - first commit

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+/**
+ * Created by rubtsovi on 10.01.2017.
+ */
+$(document).ready(function () {
+
+//    Top slider
+    $('#top-slider').carousel({
+        interval : 4000
+    });
+
+    $('#promo-slider').carousel({
+        interval: 3000
+    })
+
+})

9e33cf6fb69f71e69470b5a615cfa8a79f6dc939
rubtsovi <rubtsovi.90@gmail.com>
2017-01-11 14:34:25 +0100

    slider styling finish

--- js/custom.js
+++ js/custom.js
@@ -10,6 +10,28 @@ $(document).ready(function () {
 
     $('#promo-slider').carousel({
         interval: 3000
-    })
+    });
+    MobileMenu();
 
+
+});
+
+//scroll dla naglowka
+$(window).on('scroll',function () {
+    if($(window).scrollTop() > 20){
+        $('.main-header').addClass('header-short');
+    } else {
+        $('.main-header').removeClass('header-short');
+    }
 })
+
+function MobileMenu() {
+    $('.mobile-nav-button').on('click', function (e) {
+        e.preventDefault();
+        $('#main-nav-mobile').toggleClass('active');
+    });
+    $('#mobile-close').on('click', function (e) {
+        e.preventDefault();
+        $('#main-nav-mobile').removeClass('active');
+    })
+}
